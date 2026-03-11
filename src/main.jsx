41dba1dc4c46f6a8d24d7400510a65e44f05f3d6
Robertise <huydo170705@gmail.com>
2026-03-11 20:48:37 +0700

    fix router basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import "./index.css";
 import Layout from "./components/Layout";
 import BlogDetail from "./pages/BlogDetail";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />
+    },
+    {
+      path: "/blogs/:slug",
+      element: <BlogDetail />
+    }
+  ],
   {
-    path: "/",
-    element: <Layout />
-  },
-  {
-    path: "/blogs/:slug",
-    element: <BlogDetail />
+    basename: "/portfolio"
   }
-]);
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
